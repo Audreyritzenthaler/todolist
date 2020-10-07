58e5de61fb8045de2efe9e8f4aff9740413a0536
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-10-07 14:17:45 +0200

    create component check

---
+++ src/components/toDoItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const toDoItem = () => {
+
+  return (
+    <div>
+      <input type='checkbox' />
+      <p>Your to do item</p>
+    </div>
+  )
+}
+
+export default toDoItem
---
+++ src/components/toDoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import toDoItem from './toDoItem'
+
+const toDoList = () => {
+
+  return (
+    <div>
+      <toDoItem />
+    </div>
+  )
+}
+
+export default toDoList
