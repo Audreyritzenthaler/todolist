4d366f4364095873f9cf749a324331f08463b001
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-10-07 15:17:00 +0200

    call the good el

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ToDoList from './components/ToDoList'
 import './App.css'
 
 const App = () => {
 
   return (
     <div className="App">
+      <ToDoList />
     </div>
   )
 }
