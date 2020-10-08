2de5a606c07ae81f48a27d2f37cbcdd9bf9f166b
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-10-08 11:56:24 +0200

    create function add item + increment id

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react'
 import ToDoItem from './ToDoItem'
 
 const ToDoList = () => {
+  const [todo, setTodo] = useState({
+    id: '',
+    text: null,
+    complete: false
+  })
   const [todosData, setToDosData] = useState([
     {id: 1, text: 'Make money', complete: false},
-    {id: 2, text: 'Make love', complete: true},
+    {id: 2, text: 'Make code', complete: true},
     {id: 3, text: 'Make peace', complete: false},
     {id: 4, text: 'Make good dishes', complete: true}
   ])
@@ -20,11 +25,21 @@ const ToDoList = () => {
     setToDosData(todosTmp)
   }
 
+  const addTodo = (e) => {
+    const todosTmp = [...todosData]
+    todosTmp.push(todo)
+    setToDosData(todosTmp)
+  }
+
   return (
     <div className='listTodo'>
       { todosData.map((todoData, i) =>
         <ToDoItem key={i} id={todoData.id} complete={todoData.complete} text={todoData.text} checkFunction={checkFunc}/>)
       }
+      <div>
+        <input name='add' value={todo.text} onChange={e => setTodo({ ...todo, text: e.target.value, id: todosData.length +1 })}/>
+        <button onClick={e => addTodo(e)}>add todo</button>
+    </div>
     </div>
   )
 }
