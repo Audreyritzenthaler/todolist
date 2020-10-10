994a22f49d8d32d60c463042010613aa7b665d8a
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-10-10 18:39:02 +0200

    initialize project

---
+++ back/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+// const routes = require('./routes/index')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+// app.use('/nameRoute', routes.nameRoute)
+
+app.get('/', (req, res) => {
+  return res.status(200)
+})
+
+
+app.listen(process.env.PORT, error => {
+  if (error) {
+    console.log('Something bad happened...', error)
+  } else {
+    console.log(`Server is listening on port ${process.env.PORT}`)
+  }
+})
